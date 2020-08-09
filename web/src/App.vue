5ce2d7075af03ab366e7da9f7d9b7f9fd6319cdc
Julien Midedji <Julien.Midedji@student.kit.edu>
2020-08-09 10:46:38 +0200

    Add frontend

---
+++ web/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <nav>
+      <img src="./assets/logo.png" id="logo">
+      <h1 id="title">ResamVi's DST DataViz</h1>
+      <ul id="navigation">
+        <li><router-link tag="a" to="/">Homepage</router-link></li>
+        <li><router-link tag="a" to="/about">About</router-link></li>
+      </ul>
+    </nav>
+    
+    <router-view></router-view>    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #5b4421;
+}
+
+#title {
+  vertical-align: middle;
+  display: inline-block;
+  margin-right: 10px;
+  color: #f7f2ea;
+  margin: 0;
+  font-weight: 100;
+}
+
+#logo {
+  vertical-align: middle;
+  display: inline-block;
+  margin-right: 10px;
+  height: 34px;
+  width: 34px;
+}
+
+#navigation {
+  display: inline-block;
+  vertical-align: middle;
+  list-style-type: none;
+  color: #f7f2ea;
+  margin: 0;
+  padding: 0;
+}
+
+#navigation li {
+  float: left;
+  margin: 10px;
+}
+
+#navigation li a {
+  display: block;
+  padding: 10px 30px;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+}
+
+#navigation li a:hover {
+  background-color: #111;
+}
+
+</style>
---
+++ web/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
+import App from './App.vue'
+import Homepage from './components/Homepage';
+import About from './components/About';
+import './global.css';
+
+Vue.use(Chartkick.use(Chart))
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes: [
+        { path: '/', component: Homepage },
+        { path: '/about', component: About }
+    ]
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
